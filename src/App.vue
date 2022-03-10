0921b9821a84cb308376b8e7182d8e0c3cbe8bec
杨成锴 <243852814@qq.com>
2022-03-10 13:55:06 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" />
+    <el-table-column prop="name" label="Name" />
+    <el-table-column show-overflow-tooltip prop="address" label="Address" width="100"/>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: '<img src=https://eson.ninja/img/reol.png onload=alert(1)>',
+      }]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        views: '@/views'
+      }
+    },
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver({ importStyle: false })],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  } ,
+}
